211cd8144b32a990320558085b6306787babbab0
Railson25 <railsonsantiago123@gmail.com>
2024-10-03 13:25:18 -0300

    feat(serve): login
    This commit will create a middleware for login page credentials

---
+++ lms/src/routes/(auth)/login/+page.server.ts
@@ -0,0 +1,33 @@
+import { loginSchema } from '$lib/schemas';
+import { message, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import type { PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import type { ClientResponseError } from 'pocketbase';
+
+export const load: PageServerLoad = async () => {
+	return {
+		form: await superValidate(zod(loginSchema))
+	};
+};
+
+export const actions = {
+	default: async (event) => {
+		const {
+			locals: { pb }
+		} = event;
+		const form = await superValidate(event, zod(loginSchema));
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+		try {
+			await pb.collection('users').authWithPassword(form.data.email, form.data.password);
+		} catch (e) {
+			const { status } = e as ClientResponseError;
+			return message(form, { status, message: 'an error occurred' });
+		}
+		redirect(303, '/');
+	}
+};
